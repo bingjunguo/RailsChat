65a338684c31b04b1a11c28f2a5b53975398ec1c
bingjunguo <guobingjun1995@163.com>
2018-01-07 11:41:57 +0800

    Creat and show micropost

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
   def home
+  	@user = current_user
+  	@micropost = current_user.microposts.build if logged_in?
+  	@microposts = @user.microposts.paginate(page: params[:page]) if logged_in?
   end
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,24 @@
 class MicropostsController < ApplicationController
 
 	before_action :logged_in, only: [:create, :destroy]
+
+	def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "微博建立成功!"
+      redirect_to root_url
+    else
+    	flash[:warning] = "未发布"
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
