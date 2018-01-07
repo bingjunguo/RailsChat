82083962378fcb3d48ed295ea4191ebfe51d7606
bingjunguo <guobingjun1995@163.com>
2018-01-07 12:47:20 +0800

    Add micropost picture

--- Gemfile
+++ Gemfile
@@ -8,7 +8,11 @@ gem 'faker', '~> 1.6.3'
 gem 'rails_admin', '~> 0.8.1'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
+# 郭炳军添加
 gem 'nokogiri',                '1.6.8.1'
+gem 'carrierwave',             '1.1.0'
+gem 'mini_magick',             '4.7.0'
+gem 'fog-aws',                 '2.0.0'
 
 # for download
 gem 'paperclip'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107011622) do
+ActiveRecord::Schema.define(version: 20180107040411) do
 
   create_table "chats", force: :cascade do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20180107011622) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
